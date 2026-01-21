e4c138abdcf964861a88c7ff7de83ec391868bf7
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-01-21 17:51:34 +0100

    Initial Commit

---
+++ cpp-parallel-primes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-parallel-primes
+//
+//  Created by Sofiane Medhouny Laouina on 21/01/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
